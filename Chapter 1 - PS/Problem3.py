e306b446bcbd699103d8a9308777cff7203eff06
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-23 14:51:45 +0530

    Update Problem3.py

--- Chapter 1 - PS/Problem3.py
+++ Chapter 1 - PS/Problem3.py
@@ -1,4 +1,7 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Hey I am good")
-engine.runAndWait()
+import pyttsx3          # Text-to-Speech लाइब्रेरी इंपोर्ट की
+engine = pyttsx3.init() # pyttsx3 का एक इंजन ऑब्जेक्ट बनाया
+engine.say("Hey I am good")  # जो टेक्स्ट बोलना है, वह पास किया
+engine.runAndWait()     # ऑडियो को प्ले करने के लिए यह कमांड जरूरी है
+
+
+
