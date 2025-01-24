aa99b3d8155f288c1d3b84c61d3062e66c5bd8ed
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-24 18:54:12 +0530

    Update 04_set_methods.py

--- Chapter 5/04_set_methods.py
+++ Chapter 5/04_set_methods.py
@@ -1,8 +1,21 @@
-s = {1, 5, 32, 54,5, 5, 5, "Harry"}
+s = {1, 5, 32, 54,5, 5, 5, "suresh"}
 
 print(s, type(s))
 
-s.add(566)
-print(s, type(s))
+s.add(77)
+print(s)
+
+
 s.remove(1)
-print(s, type(s))
+print(s)
+
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+
+print(A | B)  # Union: {1, 2, 3, 4, 5}
+print(A & B)  # Intersection: {3}
+print(A - B)  # Difference: {1, 2}
+print(A ^ B)  # Symmetric Difference: {1, 2, 4, 5}
+
