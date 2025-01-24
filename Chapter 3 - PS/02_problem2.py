b37d4d88b8efe4cc90bc0ec835e4a7ad2de4795e
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-24 12:07:00 +0530

    Update 02_problem2.py

--- Chapter 3 - PS/02_problem2.py
+++ Chapter 3 - PS/02_problem2.py
@@ -1,5 +1,43 @@
+
+# place holder use 
 letter = '''Dear <|Name|>, 
 You are selected! 
 <|Date|> '''
 
-print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
+# जब आपको यह संदेश किसी को भेजना हो, तो आप नाम और तारीख को बदल सकते हैं।
+print(letter.replace("<|Name|>", "Suresh").replace("<|Date|>", "24 September 2050"))
+
+
+
+# f'' string use 
+name = "Suresh"
+date = "24 September 2050"
+letter = f'''Dear {name}, 
+You are selected! 
+{date}'''
+
+print(letter)
+
+
+
+letter = '''Dear {name}, 
+You are selected! 
+{date}'''
+
+print(letter.format(name="Suresh", date="24 September 2050"))
+
+
+
+
+
+
+#This is for sefty parpach 
+#from string import Template
+
+#name = "Suresh"
+#date = "24 September 2050"
+#letter_template = Template('''Dear $name,
+#You are selected!
+#$date''')
+
+#print(letter_template.substitute(name=name, date=date))
