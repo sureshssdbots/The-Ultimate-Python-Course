c2fb4a04de1302c646cc0a0228fed9a5f87e0e07
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-24 09:27:17 +0530

    Update 02_problem2.py

--- Chapter 2 - PS/02_problem2.py
+++ Chapter 2 - PS/02_problem2.py
@@ -2,4 +2,6 @@ a = 37
 
 b = 5
 
-print("Remainder when a is divided by b is", a % b)
+# Perform modulo operation and print the result
+# The % operator gives the remainder when 'a' is divided by 'b'
+print(a % b)  # Output: 0, क्योंकि 35 को 5 से पूरी तरह से विभाजित किया जा सकता है
