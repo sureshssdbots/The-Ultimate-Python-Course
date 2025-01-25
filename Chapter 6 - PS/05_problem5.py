8ed5abce57d8c3328a1644bf311e6e0a120a1c8d
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-25 14:43:00 +0530

    Update 05_problem5.py

--- Chapter 6 - PS/05_problem5.py
+++ Chapter 6 - PS/05_problem5.py
@@ -1,8 +1,8 @@
-l = ["Harry", "Rohan", "Shubham", "Divya"]
+l = ["Suresh", "Dilip", "Bhawana", "Rawi"]
 
-name = input("Enter your name: ")
+name = input("Enter your name: ").lower()  # .lower() को कॉल करना जरूरी है
 
-if(name in l):
+if name in [i.lower() for i in l]:  # लिस्ट के सभी नामों को lowercase में बदलकर चेक करें
     print("Your name is in the list")
 else:
     print("Your name is not in the list")
