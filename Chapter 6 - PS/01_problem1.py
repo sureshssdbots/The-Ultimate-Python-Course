006de80fbfd5d87e2666920f302ddf10c9f49186
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-25 13:22:56 +0530

    Update 01_problem1.py

--- Chapter 6 - PS/01_problem1.py
+++ Chapter 6 - PS/01_problem1.py
@@ -14,3 +14,26 @@ elif(a3>a1 and a3>a2 and a3>a4):
 
 elif(a4>a1 and a4>a2 and a4>a3):
     print("Greatest number is a4:", a4)
+    
+  
+
+# Using max() function to find the greatest number
+greatest = max(a1, a2, a3, a4)
+print("The greatest number is:", greatest)
+
+#min() is use for find the smallest number 
+smallest = min(a1, a2, a3, a4)
+print("The smallest number is:", smallest)
+
+
+# using sum() function to find all number sum
+total = sum([a1,a2,a3,a4])
+print('The sum of all number is', total)
+
+
+sorted_number =sorted([a1,a2,a3,a4])
+print('acsending oder', sorted_number)
+
+numbers = [a1,a2,a3,a4]
+numbers.reverse()
+print('Reverse oder', numbers)
