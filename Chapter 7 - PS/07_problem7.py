d76bb1f5eaa96d579024d461b6e28f75215fd179
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-26 09:46:17 +0530

    Update 07_problem7.py

--- Chapter 7 - PS/07_problem7.py
+++ Chapter 7 - PS/07_problem7.py
@@ -14,7 +14,10 @@ For n = 5
 '''
 
 n = int(input("Enter the number: "))
+
 for i in range(1, n+1):
-    print(" "* (n-i), end="")
-    print("*"* (2*i-1), end="")
-    print("")
+    print(" " * (n - i), end="")  # Spaces before stars
+    print("*" * (2 * i - 1), end="")  # Stars for the current row
+    print("")  # Move to the next line
+   
+
