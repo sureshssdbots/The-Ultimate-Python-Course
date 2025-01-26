947ca70dbc1a32ca659c9d80a5f72bc5e805ba3a
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-26 09:18:21 +0530

    Update 05_problem5.py

--- Chapter 7 - PS/05_problem5.py
+++ Chapter 7 - PS/05_problem5.py
@@ -1,8 +1,13 @@
 n = int(input("Enter the number: "))
-i = 1
-sum = 0
-while(i<=n):
-    sum += i
-    i+=1
 
-print(sum)
+if n <= 0:
+    print("Please enter a positive integer greater than 0.")
+else:
+    i = 1
+    total_sum = 0
+
+    while i <= n:
+        total_sum += i  # Add i to total_sum
+        i += 1  # Increment i by 1
+    
+    print(f"The sum of first {n} natural numbers is: {total_sum}")
