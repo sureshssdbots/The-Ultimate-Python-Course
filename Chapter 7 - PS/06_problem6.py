77f794f0d042b902a31523ac79a087916be2996d
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-26 09:26:54 +0530

    Update 06_problem6.py

--- Chapter 7 - PS/06_problem6.py
+++ Chapter 7 - PS/06_problem6.py
@@ -1,8 +1,13 @@
-# 5! = 1 X 2 X 3 X 4 X 5
-
 n = int(input("Enter the number: "))
-product = 1
-for i in range(1, n+1):
-    product = product * i
 
+if n < 0:
+    print('Please Enter positive number')
+elif n ==0:
+    print('The factorial of 0 is 1')
+
+else:
+    product= 1
+    for i in range(1, n+1):
+        product = product* i
+        
 print(f"The factorial of {n} is {product}")
