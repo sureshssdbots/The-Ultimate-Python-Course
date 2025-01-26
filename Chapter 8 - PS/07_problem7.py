320d6f42b3e4ede98f4c2b655dd28bfd65fe92ea
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-26 11:41:02 +0530

    Update 07_problem7.py

--- Chapter 8 - PS/07_problem7.py
+++ Chapter 8 - PS/07_problem7.py
@@ -1,12 +1,9 @@
-
 def rem(l, word):
-    n = [] 
-    for item in l:
-        if not(item == word):
-            n.append(item.strip(word))
-    return n
+    n = []  # एक नई खाली सूची बनाएंगे (यह वह सूची होगी जिसमें से शब्द हटा दिया गया है)
+    for item in l:  # सूची `l` में से हर आइटम को एक-एक करके लेंगे
+        if item != word:  # अगर वर्तमान आइटम `word` के बराबर नहीं है
+            n.append(item)  # तो उसे नई सूची `n` में जोड़ देंगे
+    return n  # नई सूची `n` को वापस करेंगे
 
-
-l = ["Harry", "Rohan", "Shubham", "an"]
-
-print(rem(l, "an"))
+l = ["Harry", "Rohan", "Shubham", "an"]  # यह वह मूल सूची है
+print(rem(l, "an"))  # "an" को सूची से हटाने के लिए `rem()` फंक्शन को कॉल करते हैं
