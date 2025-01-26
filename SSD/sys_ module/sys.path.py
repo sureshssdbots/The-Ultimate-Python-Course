a391450dfac17c66375cf9063012c2cfd6021805
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-26 16:01:48 +0530

    Create sys.path.py

---
+++ SSD/sys_ module/sys.path.py
@@ -0,0 +1,7 @@
+import sys
+print("Python module search paths are:", sys.path)
+
+
+#यह एक लिस्ट होती है, जो Python को यह बताती है कि मॉड्यूल को कहां से ढूंढना है।
+
+#आप इसमें कस्टम डायरेक्टरी जोड़ सकते हैं।
