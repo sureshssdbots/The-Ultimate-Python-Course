d1545b29c75948896dd5ed70821eae994051d48e
sureshssdbots <dilipdewasi7759@gmail.com>
2025-01-27 16:14:40 +0530

    Create math_module.py

---
+++ SSD/math module/math_module.py
@@ -0,0 +1,52 @@
+import math
+
+print(math.pi)  # Output: 3.141592653589793
+print(math.e)   # Output: 2.718281828459045
+
+
+print(math.factorial(5))  # Output: 120 (5! = 5*4*3*2*1)
+print(math.perm(6, 3))  # output = 120
+# math.perm(6,3)
+#     (n, k) = n! / (n-k)!
+#.     (6,3) = 6 ! / (6-3)!
+#.                = 6! /3!
+ #.                = 6*5*4
+ #.                  = 120
+
+print(math.sqrt(16))   # Output: 4.0
+print(math.pow(2, 3))  # Output: 8.0
+#math.sqrt(x): x का स्क्वायर रूट।
+ #math.pow(x, y): x की पावर y।
+ 
+ 
+
+print(math.log(10))       # Output: 2.302585092994046 (loge(10))
+print(math.log10(100))    # Output: 2.0
+ 
+# math.log(x, base): लॉगरिदम (डिफॉल्ट बेस e है)।
+#math.log10(x): बेस 10 का लॉगरिदम।
+
+
+print(math.sin(math.pi / 2))  # Output: 1.0
+print(math.cos(0))           # Output: 1.0
+print(math.tan(math.pi / 4)) # Output: 1.0
+
+
+print(math.floor(3.7))  # Output: 3
+print(math.ceil(3.2))   # Output: 4
+
+#math.floor(x) x ko नीचे की ओर राउंड करता है।
+# math.ceil(x) x ko ऊपर की ओर राउंड करता है।
+
+print(math.radians(180))  # Output: 3.141592653589793
+print(math.degrees(math.pi))  # Output: 180.0
+
+#math.radians(x)  डिग्री को रेडियन में बदलता है।
+#math.degrees(x)  रेडियन को डिग्री में बदलता है।
+
+print(math.exp(2))  # Output: 7.38905609893065
+#math.exp(x); e ki power x (e^x)
+
+
+
+
